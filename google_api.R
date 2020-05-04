d6e82e4e73f6c556ee24dba016ed2e1fbef4a819
NSYSUHermit <x5060812@gmail.com>
2020-05-04 15:52:18 +0800

    'map_pop_html'

--- google_api.R
+++ google_api.R
@@ -11,7 +11,7 @@ pd <- import("pandas")
 time <- import("time")
 requests <- import("requests")
 json <- import("json")
-gmaps = googlemaps$Client(key='AIzaSyDQ6gJ0GXB5gda8UnCn7h_2SP9FNEr16bQ')
+gmaps = googlemaps$Client(key='AIzaSyBwMa0N_fzP9WtCPpVufYzBfcMi6etyTMQ')
 
 ##########################funcion def######################################
 # Function for min.max normalize
@@ -41,7 +41,7 @@ ltd_count_num <- function(latitude,longitude,keyword,radius){
 
 # Function for find locate lon&lan function
 find_ltd <- function(locate){
-  urls <- paste0("https://maps.googleapis.com/maps/api/geocode/xml?address=",locate,"&language=zh-TW&key=AIzaSyDQ6gJ0GXB5gda8UnCn7h_2SP9FNEr16bQ")
+  urls <- paste0("https://maps.googleapis.com/maps/api/geocode/xml?address=",locate,"&language=zh-TW&key=AIzaSyBwMa0N_fzP9WtCPpVufYzBfcMi6etyTMQ")
   id_link <- read_html(urls)
   lat_lon <- c(as.numeric(html_text(html_nodes(id_link,"geometry location lat"))),as.numeric(html_text(html_nodes(id_link,"geometry location lng"))))
   return(lat_lon)
@@ -60,7 +60,7 @@ latlng_price <- function(lat1,lon1,city){
 
 # Function for find town place function
 ltd_town_find <- function(latitude,longitude){
-  urls <- paste0("https://maps.googleapis.com/maps/api/geocode/xml?latlng=",latitude,",",longitude,"&language=zh-TW&key=AIzaSyDQ6gJ0GXB5gda8UnCn7h_2SP9FNEr16bQ")
+  urls <- paste0("https://maps.googleapis.com/maps/api/geocode/xml?latlng=",latitude,",",longitude,"&language=zh-TW&key=AIzaSyBwMa0N_fzP9WtCPpVufYzBfcMi6etyTMQ")
   id_link <- read_html(urls)
   nodes <- html_nodes(id_link,"result address_component")
   return(html_text(html_nodes(nodes[grep("administrative_area_level_3",html_text(nodes))],"short_name"))[1])
@@ -68,7 +68,7 @@ ltd_town_find <- function(latitude,longitude){
 
 # Function for find village place
 ltd_vill_find <- function(latitude,longitude){
-  urls <- paste0("https://maps.googleapis.com/maps/api/geocode/xml?latlng=",latitude,",",longitude,"&language=zh-TW&key=AIzaSyDQ6gJ0GXB5gda8UnCn7h_2SP9FNEr16bQ")
+  urls <- paste0("https://maps.googleapis.com/maps/api/geocode/xml?latlng=",latitude,",",longitude,"&language=zh-TW&key=AIzaSyBwMa0N_fzP9WtCPpVufYzBfcMi6etyTMQ")
   id_link <- read_html(urls)
   nodes <- html_nodes(id_link,"result address_component")
   return(html_text(html_nodes(nodes[grep("administrative_area_level_4",html_text(nodes))],"short_name"))[1])
@@ -122,13 +122,20 @@ town_ltd_info <- function(city,town,distance,num1,num2,num3,num4,num5,num6,num7)
   t1<-Sys.time()
   for(i in c(1:nrow(a))){
     village[i] <- ltd_vill_find(a$Var1[i],a$Var2[i])
-    clinc[i] <- ltd_count_num(a$Var1[i],a$Var2[i],"clinic",num1)
-    hospital[i] <- ltd_count_num(a$Var1[i],a$Var2[i],"hospital",num2)
-    hospital_center[i] <- ltd_count_num(a$Var1[i],a$Var2[i],"hospital center",num3)
-    park[i] <- ltd_count_num(a$Var1[i],a$Var2[i],"park",num4)
-    library[i] <- ltd_count_num(a$Var1[i],a$Var2[i],"library",num5)
-    mrt_station[i] <- ltd_count_num(a$Var1[i],a$Var2[i],"mrt station",num6)
-    bus_stop[i] <- ltd_count_num(a$Var1[i],a$Var2[i],"bus stop",num7)
+    if(num1 == 0){clinc[i] = 0}
+    else{clinc[i] <- ltd_count_num(a$Var1[i],a$Var2[i],"clinic",num1)}
+    if(num2 == 0){hospital[i] = 0}
+    else{hospital[i] <- ltd_count_num(a$Var1[i],a$Var2[i],"hospital",num2)}
+    if(num3 == 0){hospital_center[i] = 0}
+    else{hospital_center[i] <- ltd_count_num(a$Var1[i],a$Var2[i],"hospital center",num3)}
+    if(num4 == 0){park[i] = 0}
+    else{park[i] <- ltd_count_num(a$Var1[i],a$Var2[i],"park",num4)}
+    if(num5 == 0){library[i] = 0}
+    else{library[i] <- ltd_count_num(a$Var1[i],a$Var2[i],"library",num5)}
+    if(num6 == 0){mrt_station[i] = 0}
+    else{mrt_station[i] <- ltd_count_num(a$Var1[i],a$Var2[i],"mrt station",num6)}
+    if(num7 == 0){bus_stop[i] = 0}
+    else{bus_stop[i] <- ltd_count_num(a$Var1[i],a$Var2[i],"bus stop",num7)}
     t2 <- Sys.time()
     time <- t2-t1
     print(paste0("Completion:",i/nrow(a),". Time cost:",time))
@@ -165,17 +172,26 @@ town_location_draw <- function(city,town,distance,num1,num2,num3,num4,num5,num6,
   ltd = read_sf("./TOWN_MOI_1090324.shp")
   df3 <- town_ltd_info(city,town,distance,num1,num2,num3,num4,num5,num6,num7)
   bdd <- get_bound_ltd(city,town)
-  df3$info <- paste(sep = "<br/>",
-                    "診所個數:",df3$clinc,
-                    "地區醫院個數:",df3$hospital,
-                    "醫學中心個數:",df3$hospital_center,
-                    "公園個數:",df3$park,
-                    "圖書館個數:",df3$library,
-                    "捷運站出口個數:",df3$mrt_station,
-                    "公車站牌個數:",df3$bus_stop,
-                    "110老化指數:",df3[,46])
+  
+  df3$index <- 0.3*(nor.min.max(df3$clinc+df3$hospital+df3$hospital_center+df3$park+df3$library+df3$mrt_station+df3$bus_stop)+1.5)/3+(3*df3[,14]+7*df3[,16])*0.003-0.05*(nor.min.max(df3$price)+1.5)
+  df3$info <- c("")
+  df3$info <- paste0(df3$info,"<font size='4'><font color='red'>區域指標:",df3$index,"</font></font><br/>")
+  if(num1 != 0){df3$info <- paste0(df3$info,"診所個數:",df3$clinc,"<br/>")}
+  if(num2 != 0){df3$info <- paste0(df3$info,"地區醫院個數:",df3$hospital,"<br/>")}
+  if(num3 != 0){df3$info <- paste0(df3$info,"醫學中心個數:",df3$hospital_center,"<br/>")}
+  if(num4 != 0){df3$info <- paste0(df3$info,"公園個數:",df3$park,"<br/>")}
+  if(num5 != 0){df3$info <- paste0(df3$info,"圖書館個數:",df3$library,"<br/>")}
+  if(num6 != 0){df3$info <- paste0(df3$info,"捷運站出口個數:",df3$mrt_station,"<br/>")}
+  if(num7 != 0){df3$info <- paste0(df3$info,"公車站牌個數:",df3$bus_stop,"<br/>")}
+  df3$info <- paste0(df3$info,"110老化指數:",df3[,46])
+  
+  
   lat_d = distance*0.000009090909
   lon_d = distance*0.00001
+  
+  rc2 <- colorRampPalette(colors = c("white", "green"), space = "Lab")(50)
+  mypal <- colorNumeric(palette =rc2, domain = df3$index)
+  
   leaflet(df3) %>% addTiles() %>%
     addPolygons(lng = bdd$V1,
                 lat = bdd$V2,
@@ -188,18 +204,12 @@ town_location_draw <- function(city,town,distance,num1,num2,num3,num4,num5,num6,
     addRectangles(
       lng1=~Var2-lon_d/2, lat1=~Var1-lat_d/2,
       lng2=~Var2+lon_d/2, lat2=~Var1+lat_d/2,
-      fillOpacity = nor.min.max(rowSums(scale(df3[,c(3:11,46)])))*0.8+0.1,
-      color = "green",#關於線條的顏色
-      stroke = FALSE,
-      group = NULL,
-      popup = ~as.factor(df3$info)#%>%
-    # addCircles(lng = ~Var2, lat = ~Var1, 
-    #            radius = distance/2, 
-    #            fillOpacity = nor.min.max(rowSums(scale(df3[,c(3:11,46)])))*0.8+0.1,
-    #            color = "green",#關於線條的顏色
-    #            stroke = FALSE,
-    #            group = NULL,
-    #            popup = ~as.factor(df3$info)
-    )
+      fillOpacity = df3$index,
+      color = "blue",#關於線條的顏色
+      fillColor =  ~mypal(df3$index),
+      popup = ~as.factor(df3$info)
+    )%>%
+    addLegend(position = "bottomright", pal = mypal, values = df3$index,title = "選址條件指標",
+              opacity = 1)
   #return(leaflet)
 }
--- google_api_example.R
+++ google_api_example.R
@@ -1,9 +1,19 @@
 source("google_api.R", encoding = "utf-8")
 
 # city, town, radius, clinc, hospital, hospital_center, park, library, mrt_station,bus_stop
-town_location_draw("高雄市","鹽埕區",500,100,1000,5000,300,300,500,100)
+town_location_draw("高雄市","鹽埕區",1000,100,1000,5000,300,300,500,100)
 
-df <- town_ltd_info("高雄市","鹽埕區",500,100,1000,5000,300,300,500,100)
+df <- town_ltd_info("高雄市","鹽埕區",1000,0,1000,5000,300,300,500,100)
+
+
+leaflet() %>% addTiles() %>%
+  addRectangles(
+    lng1=-118.456554, lat1=34.078039,
+    lng2=-118.436383, lat2=34.062717,
+    fillOpacity = 0.1,
+    color = "green"
+    #fillColor = "transparent"
+  )
 
 
 
